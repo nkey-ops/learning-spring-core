9c06237919dc6238029471f52343794cee1a4c6a
nkey-ops <terrythebear746@gmail.com>
2025-05-08 22:57:11 +0400

    bean overview registerSingleton

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,27 +1,29 @@
 package main;
 
-import java.io.File;
-
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- */
+/** Hello world! */
 public class App {
     public static void main(String[] args) {
         var annonContext = new AnnotationConfigApplicationContext();
         new XmlBeanDefinitionReader(annonContext).loadBeanDefinitions("services.xml");
-        
+
         System.out.println(annonContext);
         annonContext.refresh();
         System.out.println(annonContext);
 
-       var petStoreBean = annonContext.getBean("petStoreService", PetStoreService.class);
-       var petControllService = annonContext.getBean("petControllService", PetControllService.class);
-       System.out.println(petStoreBean);
-       System.out.println(petControllService);
+        var petStoreBean = annonContext.getBean("petStoreService", PetStoreService.class);
+        var petControllService =
+                annonContext.getBean("petControllService", PetControllService.class);
+        System.out.println(petStoreBean);
+        System.out.println(petControllService);
+
+       ((DefaultListableBeanFactory) annonContext.getAutowireCapableBeanFactory()).registerSingleton(NonRegisteredBean.class.getName(), NonRegisteredBean.class);
+
+        var nonRegBean = annonContext.getBean(NonRegisteredBean.class.getName());
+        System.out.println(nonRegBean);
     }
 }
---
+++ src/main/java/main/NonRegisteredBean.java
@@ -0,0 +1,9 @@
+package main;
+
+public class NonRegisteredBean {
+
+    @Override
+    public String toString() {
+        return "NonRegisteredBean []";
+    }
+}
